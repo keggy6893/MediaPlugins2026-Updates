f877c2f4275fd38d4b0302d82e05d548d9a06220
keggy6893 <Keggy@gmx.at>
2026-09-24 08:29:50 +0200

    Use validated shared token login for Emby

--- development/2026-09-20/MediaPlugins2026/backends/emby_client.py
+++ development/2026-09-20/MediaPlugins2026/backends/emby_client.py
@@ -13,29 +13,3 @@ class EmbyClient(JellyfinClient):
     """
 
     CLIENT_NAME = "MediaPlugins2026-Emby"
-
-    def login(self, callback, error_callback):
-        """
-        Emby: importierte Token/User-ID nicht blind weiterverwenden.
-
-        Fuer Emby wird mit den vorhandenen Zugangsdaten neu authentifiziert,
-        damit ein importierter oder abgelaufener Token nicht erst bei den
-        nachfolgenden Home-/Library-/Favorites-Requests als HTTP 401 auffaellt.
-
-        JellyfinClient bleibt unveraendert.
-        """
-        old_token = self.token
-        old_user_id = self.user_id
-
-        self.token = None
-        self.user_id = None
-
-        def login_ok(token, user_id):
-            callback(token, user_id)
-
-        def login_failed(err):
-            self.token = old_token
-            self.user_id = old_user_id
-            error_callback(err)
-
-        return super(EmbyClient, self).login(login_ok, login_failed)
